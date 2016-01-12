f46c2a8d8ec3033e349df60a3e7db8795d531071
lgs3137 <lgs3137@gmail.com>
2016-01-12 15:49:02 +0800

    Only Use For MX4Pro To Build CM13

---
+++ mobicore/Android.mk
@@ -0,0 +1,32 @@
+# =============================================================================
+#
+# Makefile pointing to all makefiles within the project.
+#
+# =============================================================================
+MOBICORE_PROJECT_PATH := $(call my-dir)
+# Setup common variables
+LOG_WRAPPER := $(MOBICORE_PROJECT_PATH)/common/LogWrapper
+COMP_PATH_MobiCore := $(MOBICORE_PROJECT_PATH)/common/MobiCore
+COMP_PATH_MobiCoreDriverMod := $(MOBICORE_PROJECT_PATH)/include
+
+
+# Application wide Cflags
+GLOBAL_INCLUDES := bionic \
+	$(COMP_PATH_MobiCore)/inc \
+	$(COMP_PATH_MobiCoreDriverMod)/Public \
+	$(COMP_PATH_MobiCore)/inc/TlCm
+
+LOCAL_CXX_STL := libstlport
+
+# Include the Daemon
+include $(MOBICORE_PROJECT_PATH)/MobiCoreDriverLib/Android.mk
+
+MC_INCLUDE_DIR := $(COMP_PATH_MobiCore)/inc \
+    $(COMP_PATH_MobiCore)/inc/TlCm \
+    $(COMP_PATH_MobiCore)/inc/TlCm/2.0 \
+    $(MOBICORE_PROJECT_PATH)/MobiCoreDriverLib/ClientLib/public \
+    $(MOBICORE_PROJECT_PATH)/MobiCoreDriverLib/Registry/Public
+MC_DEBUG := _DEBUG
+SYSTEM_LIB_DIR=/system/lib
+GDM_PROVLIB_SHARED_LIBS=libMcClient
+
---
+++ mobicore/README.md
@@ -0,0 +1,5 @@
+# trustonic-tee-user-space
+
+Android user space components for the Trustonic Trusted Execution Environment
+
+Version for the QEmu/Goldfish emulator
